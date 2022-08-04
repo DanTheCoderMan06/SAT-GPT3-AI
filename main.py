075a6b062e556a6815dfc3d6a19718726a487749
dnovakk <87329753+dnovakk@users.noreply.github.com>
2022-08-04 10:10:18 -0700

    Update and rename Main.py to main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-!pip install transformers
-!pip install sentence_transformers 
-!pip install openai
 import json
 import pandas as pd
 import sys
 import os
-sys.path.append('/files')
 from functions import *
 import math
 import random
@@ -28,14 +24,14 @@ sample_size = 1
 keys = ["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","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"]
 names = []
 #The Variables for the Program, make sure you have a folder named files under the parent of this .py file.
-df = pd.read_csv('/files/Indian-Male-Names.csv')['name']
+df = pd.read_csv('data/Indian-Male-Names.csv')['name']
 dflist = list(df)
 for name in dflist:
   try:
     names.append(name.split(' ')[0])
   except:
     pass
-df = pd.read_csv('/files/Indian-Female-Names.csv')['name']
+df = pd.read_csv('data/Indian-Female-Names.csv')['name']
 dflist = list(df)
 for name in dflist:
   try:
@@ -62,10 +58,10 @@ def constant_updates():
     distances = create_one_step_list(1) + create_two_step_list(1) 
     random.shuffle(distances)
     newdistances = pd.DataFrame(distances)
-    olddistances = pd.read_csv('/files/distances2.csv')
+    olddistances = pd.read_csv('data/distances2.csv')
     distanceframe = pd.concat([newdistances,olddistances],ignore_index=True)
     print(len(distanceframe))
-    distanceframe.to_csv('/files/distances2.csv')
+    distanceframe.to_csv('data/distances2.csv')
 while True:
   running = True
   loop = threading.Thread(target = constant_updates,args = ())
@@ -75,9 +71,9 @@ while True:
   stop = input('Are you ready to Quit? (y = yes, anything else is no)')
   if stop == 'y':
     break
-distances = pd.read_csv('/content/drive/MyDrive/distances2.csv')
+distances = pd.read_csv('data/distances2.csv')
 distanceframe = pd.DataFrame(distances)
-distanceframe.to_csv('/content/drive/MyDrive/distances2.csv')
+distanceframe.to_csv('data/distances2.csv')
 distanceframe.groupby('type',as_index = False).mean().plot.bar('type','distance')
 print(len(distanceframe))
 from scipy.stats import ttest_ind
